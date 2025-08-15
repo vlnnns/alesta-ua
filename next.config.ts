af87177f4a54f6faab893e6ed49bf93b9298203b
vlnnns <102325878+vlnnns@users.noreply.github.com>
2025-08-15 17:38:33 +0300

    deleted standalone

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     reactStrictMode: true,
     // swcMinify: true,
-    output: 'standalone',
 
     // ⚠️ TEMP: не блокировать деплой из‑за типовых/ESLint ошибок
     typescript: {
